a667be7a302e55e97963d1f4ff2e5a128c9a1046
gabrielonishi <gabriel.onishi@outlook.com>
2023-05-24 22:17:07 -0300

    Conclui endpoint de leitura de arquivos

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -3,17 +3,17 @@ resource "aws_api_gateway_rest_api" "s3-management-api" {
   binary_media_types = ["application/json"]
 }
 
-resource "aws_api_gateway_resource" "upload" {
-  parent_id   = aws_api_gateway_rest_api.s3-management-api.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.s3-management-api.id
-  path_part   = "upload"
-}
+# resource "aws_api_gateway_resource" "upload" {
+#   parent_id   = aws_api_gateway_rest_api.s3-management-api.root_resource_id
+#   rest_api_id = aws_api_gateway_rest_api.s3-management-api.id
+#   path_part   = "upload"
+# }
 
-resource "aws_api_gateway_resource" "cid" {
-  parent_id   = aws_api_gateway_rest_api.s3-management-api.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.s3-management-api.id
-  path_part   = "{upload}"
-}
+# resource "aws_api_gateway_resource" "cid" {
+#   parent_id   = aws_api_gateway_rest_api.s3-management-api.root_resource_id
+#   rest_api_id = aws_api_gateway_rest_api.s3-management-api.id
+#   path_part   = "{cid}"
+# }
 
 resource "aws_api_gateway_method" "get-filenames" {
   authorization = "NONE"
@@ -32,13 +32,39 @@ resource "aws_api_gateway_integration" "read-filenames-lambda-integration" {
   uri                     = aws_lambda_function.read-filenames-lambda.invoke_arn
 }
 
+# Da permissão ao API Gateway para invocar a função Lambda
 resource "aws_lambda_permission" "read-filenames-lambda-permission" {
   statement_id  = "AllowS3ManagementAPIInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.read-filenames-lambda.function_name
   principal     = "apigateway.amazonaws.com"
 
-  # The /* part allows invocation from any stage, method and resource path
-  # within API Gateway.
   source_arn = "${aws_api_gateway_rest_api.s3-management-api.execution_arn}/*"
+}
+
+resource "aws_api_gateway_deployment" "deploy-api" {
+  rest_api_id = aws_api_gateway_rest_api.s3-management-api.id
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      # aws_api_gateway_resource.example.id,
+      aws_api_gateway_method.get-filenames.id,
+      aws_api_gateway_integration.read-filenames-lambda-integration.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "dev-stage" {
+  deployment_id = aws_api_gateway_deployment.deploy-api.id
+  rest_api_id   = aws_api_gateway_rest_api.s3-management-api.id
+  stage_name    = "dev"
+}
+
+output "api-url" {
+  # value = aws_api_gateway_deployment.deploy-api.invoke_url
+  value = aws_api_gateway_stage.dev-stage.invoke_url
 }
