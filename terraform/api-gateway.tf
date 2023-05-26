884f93e7732021fa938f3585a21deab313c4e81f
gabrielonishi <gabriel.onishi@outlook.com>
2023-05-26 17:30:39 -0300

    Adiciona barreira com API Keys

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -1,150 +1,3 @@
-# resource "aws_api_gateway_rest_api" "s3-management-api" {
-#   name               = "s3-management-api"
-#   binary_media_types = ["application/json"]
-# }
-
-# resource "aws_api_gateway_resource" "cid" {
-#   parent_id   = aws_api_gateway_rest_api.s3-management-api.root_resource_id
-#   rest_api_id = aws_api_gateway_rest_api.s3-management-api.id
-#   path_part   = "{cid}"
-# }
-
-# resource "aws_api_gateway_resource" "upload" {
-#   parent_id   = aws_api_gateway_rest_api.s3-management-api.root_resource_id
-#   rest_api_id = aws_api_gateway_rest_api.s3-management-api.id
-#   path_part   = "upload"
-# }
-
-# resource "aws_api_gateway_method" "get-filenames" {
-#   authorization = "NONE"
-#   http_method   = "GET"
-#   resource_id   = aws_api_gateway_rest_api.s3-management-api.root_resource_id
-#   rest_api_id   = aws_api_gateway_rest_api.s3-management-api.id
-# }
-
-# resource "aws_api_gateway_method" "get-file" {
-#   authorization = "NONE"
-#   http_method   = "GET"
-#   resource_id   = aws_api_gateway_resource.cid.id
-#   rest_api_id   = aws_api_gateway_rest_api.s3-management-api.id
-# }
-
-# resource "aws_api_gateway_method" "post-file" {
-#   authorization = "NONE"
-#   http_method   = "POST"
-#   resource_id   = aws_api_gateway_resource.upload.id
-#   rest_api_id   = aws_api_gateway_rest_api.s3-management-api.id
-# }
-
-# resource "aws_api_gateway_integration" "read-filenames-lambda-integration" {
-#   rest_api_id = aws_api_gateway_rest_api.s3-management-api.id
-#   resource_id = aws_api_gateway_method.get-filenames.resource_id
-#   http_method = aws_api_gateway_method.get-filenames.http_method
-
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri                     = aws_lambda_function.read-filenames-lambda.invoke_arn
-# }
-
-# resource "aws_api_gateway_integration" "get-file-lambda-integration" {
-#   rest_api_id = aws_api_gateway_rest_api.s3-management-api.id
-#   resource_id = aws_api_gateway_method.get-file.resource_id
-#   http_method = aws_api_gateway_method.get-file.http_method
-
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri                     = aws_lambda_function.read-file-lambda.invoke_arn
-# }
-
-# resource "aws_api_gateway_integration" "post-file-lambda-integration" {
-#   rest_api_id = aws_api_gateway_rest_api.s3-management-api.id
-#   resource_id = aws_api_gateway_method.post-file.resource_id
-#   http_method = aws_api_gateway_method.post-file.http_method
-
-#   integration_http_method = "POST"
-#   type                    = "AWS"
-#   uri                     = aws_lambda_function.write-file-lambda.invoke_arn
-#   request_templates = {
-#     "application/json" = "${file("api_gateway_body_mapping.template")}"
-#   }
-#   passthrough_behavior = "WHEN_NO_TEMPLATES"
-# }
-
-# resource "aws_api_gateway_method_response" "response_200" {
-#   depends_on  = [aws_api_gateway_integration.post-file-lambda-integration]
-#   rest_api_id = aws_api_gateway_rest_api.s3-management-api.id
-#   resource_id = aws_api_gateway_resource.upload.id
-#   http_method = aws_api_gateway_method.post-file.http_method
-#   status_code = "200"
-
-#   response_models = {
-#     "application/json" = "Empty"
-#   }
-# }
-
-# resource "aws_api_gateway_integration_response" "post-integration-response" {
-#   rest_api_id = aws_api_gateway_rest_api.s3-management-api.id
-#   resource_id = aws_api_gateway_resource.upload.id
-#   http_method = aws_api_gateway_method.post-file.http_method
-#   status_code = aws_api_gateway_method_response.response_200.status_code
-# }
-
-# resource "aws_lambda_permission" "read-filenames-lambda-permission" {
-#   statement_id  = "AllowAPIReadFilenames"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.read-filenames-lambda.function_name
-#   principal     = "apigateway.amazonaws.com"
-
-#   source_arn = "${aws_api_gateway_rest_api.s3-management-api.execution_arn}/*"
-# }
-
-# resource "aws_lambda_permission" "read-file-lambda-permission" {
-#   statement_id  = "AllowAPIReadBucket"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.read-file-lambda.function_name
-#   principal     = "apigateway.amazonaws.com"
-
-#   source_arn = "${aws_api_gateway_rest_api.s3-management-api.execution_arn}/*"
-# }
-
-# resource "aws_lambda_permission" "write-file-lambda-permission" {
-#   statement_id  = "AllowAPIWriteBucket"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.write-file-lambda.function_name
-#   principal     = "apigateway.amazonaws.com"
-
-#   source_arn = "${aws_api_gateway_rest_api.s3-management-api.execution_arn}/*"
-# }
-
-# resource "aws_api_gateway_deployment" "deploy-api" {
-#   rest_api_id = aws_api_gateway_rest_api.s3-management-api.id
-
-#   triggers = {
-#     redeployment = sha1(jsonencode([
-#       # aws_api_gateway_resource.example.id,
-#       aws_api_gateway_method.get-filenames.id,
-#       aws_api_gateway_integration.read-filenames-lambda-integration.id,
-#       aws_api_gateway_integration.get-file-lambda-integration.id,
-#       aws_api_gateway_integration.post-file-lambda-integration.id,
-#     ]))
-#   }
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
-
-# resource "aws_api_gateway_stage" "dev-stage" {
-#   deployment_id = aws_api_gateway_deployment.deploy-api.id
-#   rest_api_id   = aws_api_gateway_rest_api.s3-management-api.id
-#   stage_name    = "dev"
-# }
-
-# output "api-url" {
-#   # value = aws_api_gateway_deployment.deploy-api.invoke_url
-#   value = aws_api_gateway_stage.dev-stage.invoke_url
-# }
-
 resource "aws_api_gateway_rest_api" "client_log_management_API" {
   name               = "client_log_management_API"
   binary_media_types = ["application/json"]
@@ -161,6 +14,7 @@ resource "aws_api_gateway_method" "proxy" {
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
   authorization = "NONE"
+  api_key_required = true
 }
 
 resource "aws_api_gateway_integration" "lambda" {
@@ -178,6 +32,7 @@ resource "aws_api_gateway_method" "proxy_root" {
   resource_id   = aws_api_gateway_rest_api.client_log_management_API.root_resource_id
   http_method   = "ANY"
   authorization = "NONE"
+  api_key_required = true
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
@@ -197,6 +52,25 @@ resource "aws_lambda_permission" "api_gw" {
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_api_gateway_rest_api.client_log_management_API.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_usage_plan" "standard_usage_plan" {
+  name = "standard_usage_plan"
+
+  api_stages {
+    api_id = aws_api_gateway_rest_api.client_log_management_API.id
+    stage  = aws_api_gateway_stage.dev-stage.stage_name
+  }
+}
+
+resource "aws_api_gateway_api_key" "api_access_key" {
+  name = "api_access_key"
+}
+
+resource "aws_api_gateway_usage_plan_key" "main" {
+  key_id        = aws_api_gateway_api_key.api_access_key.id
+  key_type      = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.standard_usage_plan.id
 }
 
 resource "aws_api_gateway_deployment" "deploy-api" {
@@ -224,6 +98,9 @@ resource "aws_api_gateway_stage" "dev-stage" {
 }
 
 output "api-url" {
-  # value = aws_api_gateway_deployment.deploy-api.invoke_url
   value = aws_api_gateway_stage.dev-stage.invoke_url
+}
+
+output "api-key" {
+  value = aws_api_gateway_usage_plan_key.main.value
 }
